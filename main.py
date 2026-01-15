7570770fbc6cc367d9c6b8d9b0d648c72cef0a86
Ashish Pandey <pandey.k.ashish.86@gmail.com>
2026-01-15 22:54:19 +0530

    feat: allow all origins in CORS middleware configuration.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000/", "https://frontend-indol-kappa-34.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
